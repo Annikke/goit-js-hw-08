beb73f386e595acb1e0cc4bef5c6097d18d80177
Anna Sova <sova.ania@gmail.com>
2023-01-28 18:32:47 +0100

    task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,14 @@
 import Player from "@vimeo/player";
 import {throttle} from "lodash";
 
+const STORAGE_KEY = "videoplayer-current-time";
+
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
 const onTimeUpdate = throttle(function (data) {
   try {
-    localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
   } catch (error) {
     console.error(error);
   };
@@ -16,7 +18,7 @@ player.on('timeupdate', onTimeUpdate);
 
 const getSavedTime = function () {
   try {
-    const savedTime = localStorage.getItem("videoplayer-current-time");
+    const savedTime = localStorage.getItem(STORAGE_KEY);
     return JSON.parse(savedTime).seconds;
   } catch (error) {
     console.error(error);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,49 @@
+import { throttle } from "lodash";
 
+const STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+const form = document.querySelector('.feedback-form');
+
+checkStorage();
+
+form.addEventListener('input', throttle(saveFeedback, 500));
+form.addEventListener("submit", formSubmit);
+
+function saveFeedback(eve) {
+    formData [eve.target.name] = eve.target.value;
+    save(STORAGE_KEY, formData);
+};
+
+function formSubmit(eve) {
+    eve.preventDefault();
+    eve.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    console.log(formData);
+};
+
+function checkStorage(eve) {
+    const storageState = load(STORAGE_KEY);
+    if (storageState) {
+        form.email.value = storageState.email;
+        form.message.value = storageState.message;
+    };
+};
+
+function save(key, value) {
+  try {
+    const serializedData = JSON.stringify(value);
+    localStorage.setItem(key, serializedData);
+  } catch (err) {
+    console.error(err);
+    };
+};
+
+function load(key) {
+    try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (err) {
+    console.error(err);
+    };
+};
